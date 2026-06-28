0a9b10149d9a64cf07b708c3247cb474d607abbf
Arjun Rathore <arjun.rathore1812@gmail.com>
2026-06-28 22:53:33 +0530

    Initial Commit

--- Server/server.js
+++ Server/server.js
@@ -20,7 +20,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/tasks", taskRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
