6cdd8f87deb32c8a39de9e84352a282510a65aa8
Arjun Rathore <arjun.rathore1812@gmail.com>
2026-06-29 22:25:40 +0530

    initial commit

--- Server/config/db.js
+++ Server/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
     try {
-        console.time("Mongo Connection");
         await mongoose.connect(process.env.MONGO_URI);
-        console.timeEnd("Mongo Connection");
         console.log("MongoDB Connected");
     } catch (error) {
         console.log(error.message);
